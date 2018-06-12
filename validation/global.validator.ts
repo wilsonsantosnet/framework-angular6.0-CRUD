66994904719d22db8059eddbd97276e11d82c7ed
wilsonsantosnet <wilsonsantosnet@gmail.com>
2018-06-12 09:43:30 -0300

    ++

---
+++ validation/global.validator.ts
@@ -0,0 +1,22 @@
+﻿
+export class GlobalValidator {
+
+    static mailFormat(control: any): ValidationResult {
+
+        var valor = control.value || "";
+
+        var EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (valor != "" && (valor.length <= 5 || !EMAIL_REGEXP.test(valor))) {
+
+            return { "incorrectMailFormat": true };
+        }
+
+        return null;
+    }
+
+}
+
+interface ValidationResult {
+    [key: string]: boolean;
+}
