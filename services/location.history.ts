6c73cc2dc30b13f1328af287afe1868cd6c83b99
Wilson Santos <wilsonsantosnet@gmail.com>
2018-08-24 17:33:04 -0300

    add resourceName

--- services/location.history.ts
+++ services/location.history.ts
@@ -1,16 +1,16 @@
-﻿import { Injectable } from '@angular/core';
-
+import { Injectable } from '@angular/core';
 import { CacheService } from './cache.service';
 import { ECacheType } from '../../common/type-cache.enum';
+
 
 @Injectable()
 export class LocationHistoryService {
 
-    public static saveLocal(resourceName: string) {
-        CacheService.add("lastNavigation", resourceName, ECacheType.COOKIE, 1);
-    }
+  public static saveLocal(resourceName: string, url: string) {
+    CacheService.add("lastNavigation" + resourceName, url, ECacheType.LOCAL);
+  }
 
-    public static getLastNavigation() {
-        return CacheService.get("lastNavigation", ECacheType.COOKIE)
-    }
+  public static getLastNavigation(resourceName: string) {
+    return CacheService.get("lastNavigation" + resourceName, ECacheType.LOCAL)
+  }
 }
