16ec25e4562216abd62a9ffd6eb76bd9eda6d7e6
Wilson Santos <wilsonsantosnet@gmail.com>
2018-08-28 12:37:40 -0300

    ++

--- components/cpf-cnpj.component.ts
+++ components/cpf-cnpj.component.ts
@@ -10,21 +10,24 @@ import { ViewModel } from '../model/viewmodel';
               <div class="input-group">
                 <div class="input-group-prepend">
                   <div class="input-group-text">
-                    <input type="radio" [(ngModel)]='tpCTRLcpfCnpj'  [value]='1' name='tpCTRLcpfCnpj' [ngModelOptions]="{standalone: true}">&nbsp;CPF ou &nbsp;
-                    <input type="radio" [(ngModel)]='tpCTRLcpfCnpj' [value]='2' name='tpCTRLcpfCnpj' [ngModelOptions]="{standalone: true}">&nbsp;CNPJ?
+                    <input type="radio" [(ngModel)]='tpCTRLcpfCnpj'  [value]='1' name='tpCTRLcpfCnpj' [ngModelOptions]="{standalone: true}" (change)="onChangeTipo($event)">&nbsp;CPF ou &nbsp;
+                    <input type="radio" [(ngModel)]='tpCTRLcpfCnpj' [value]='2' name='tpCTRLcpfCnpj' [ngModelOptions]="{standalone: true}" (change)="onChangeTipo($event)">&nbsp;CNPJ?
                   </div>
                 </div>
-                <input *ngIf='tpCTRLcpfCnpj==1 || !tpCTRLcpfCnpj' type='text' class='form-control' [(ngModel)]='vm.model[fieldName]' name="{{fieldName}}" formControlName="{{fieldName}}" [textMask]='{mask: vm.masks.maskCPF}' required="{isRequired}"/>
-                <input *ngIf='tpCTRLcpfCnpj==2' type='text' class='form-control' [(ngModel)]='vm.model[fieldName]' name="{{fieldName}}" formControlName="{{fieldName}}" [textMask]='{mask: vm.masks.maskCNPJ}' required="{isRequired}"/>
+                <input *ngIf='tpCTRLcpfCnpj==1 || !tpCTRLcpfCnpj' type='text' class='form-control' [(ngModel)]='vm.model[fieldName]' name="{{fieldName}}" formControlName="{{fieldName}}" [textMask]='{mask: vm.masks.maskCPF}' required="{isRequired}" (change)="onChangeCpf($event)" />
+                <input *ngIf='tpCTRLcpfCnpj==2' type='text' class='form-control' [(ngModel)]='vm.model[fieldName]' name="{{fieldName}}" formControlName="{{fieldName}}" [textMask]='{mask: vm.masks.maskCNPJ}' required="{isRequired}" (change)="onChangeCnpj($event)"/>
               </div>
             </div>`
 })
 export class CpfCnpjComponent implements OnInit {
 
   @Input() vm: ViewModel<any>
-  tpCTRLcpfCnpj: number;
   @Input() fieldName: string;
   @Input() isRequired: boolean;
+
+  tpCTRLcpfCnpj: number;
+  cpf: string;
+  cnpj: string;
 
   constructor() {
   }
@@ -33,7 +36,21 @@ export class CpfCnpjComponent implements OnInit {
     this.tpCTRLcpfCnpj = 1;
   }
 
-  
+  onChangeTipo(e: any) {
+    if (this.tpCTRLcpfCnpj == 1)
+      this.vm.model[this.fieldName] = this.cpf;
+    else {
+      this.vm.model[this.fieldName] = this.cnpj;
+    }
+  }
+
+  onChangeCpf(e: any) {
+    this.cpf = this.vm.model[this.fieldName];
+  }
+
+  onChangeCnpj(e: any) {
+    this.cnpj = this.vm.model[this.fieldName];
+  }
 
 
 }
